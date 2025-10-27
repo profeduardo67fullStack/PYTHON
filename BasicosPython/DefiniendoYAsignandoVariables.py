f6bfba98d4375a65b205ceca6cff210f5bc20914
ProfEduardo67 <profeduardo67.cap@gmail.com>
2025-10-27 14:19:38 -0600

    Asignación de variables

---
+++ BasicosPython/DefiniendoYAsignandoVariables.py
@@ -0,0 +1,24 @@
+'''
+No: 1
+Tema: Declaración y Asignación de variables
+Autor: Prof. Eduardo67
+Fecha: 27/10/2025
+'''
+
+
+# Definición y asignación de variables
+
+nombre = "Eduardo"
+edad = 58
+altura = 1.70
+ocupacion = "Docente"
+es_estudiante_tambien = True
+
+print("Resumen del usuario")
+print("Nombre: " , nombre)
+print("Edad: " , edad)
+print("Altura: " , altura)
+print("Ocupación: " , ocupacion)
+print("Es estudiante tambien: " , es_estudiante_tambien)
+
+
