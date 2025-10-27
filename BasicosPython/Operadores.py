53f43b9c589601be508ca153378973a8ba5ea8a6
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-10-27 14:52:01 -0600

    Hasta aca los operadores relacionales

--- BasicosPython/Operadores.py
+++ BasicosPython/Operadores.py
@@ -16,3 +16,21 @@ print("x < y = ", x<y) # True
 print("x >= y = ", x>=y) # False
 print("x <= y = ", x<=y) # True
 
+
+#Notemos que para tipos Boolenos True es igual a 1
+
+print(True == 1) # True
+print(True > 0.999) # True
+
+print("abc" < "abd") # True
+print("A" < "a") # True
+
+#Por que el valor unicode de A es menor al valor unicode de a
+
+print("El valor Unicode o ASCII de la A es: ", ord('A')) #65
+print("El valor Unicode o ASCII de la A es: ", ord('a')) #97
+
+#Tambien podemos comparar un vector (Coordenadas)
+
+print([3,4] >= [3,5]) #False
+
