5da62d5fa806512b2d6ac29ff4476199c0e10ff1
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-10-27 14:39:51 -0600

    Operadores Relacionales

---
+++ BasicosPython/Operadores.py
@@ -0,0 +1,18 @@
+'''
+No: 2
+Tema: Probando los operadores
+Autor: Prof. Eduardo67
+Fecha: 27/10/2025
+
+'''
+
+x=2; y=3
+
+print("Operadores Relacionales con los valores: x = " , 2 , "y = " , y)
+print("x == y = ", x==y) # False
+print("x != y = ", x!=y) # True
+print("x > y = ", x>y) # False
+print("x < y = ", x<y) # True
+print("x >= y = ", x>=y) # False
+print("x <= y = ", x<=y) # True
+
