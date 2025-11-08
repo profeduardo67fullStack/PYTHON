0291c3ea6b17c5468b578390bfcd334fb8dccf70
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-08 13:41:52 -0600

    Impresón con formatos de salida sep y end

---
+++ BasicosPython/ParametrosDeImpresion.py
@@ -0,0 +1,22 @@
+'''
+No: 4
+Tema: Parametrso de impresión determinandoparámetros sep y end
+Autor: Prof. Eduardo67
+Fecha: 27/10/2025
+
+'''
+
+
+print ("Hola", "Como estas", "Tu", "en", "Apizaco")
+print ("Hola", "Como estas", sep=", ",  end="! ")
+
+#Imprimir en la misma línea
+
+print("Imresión", end=" ")
+print("en", end=" ")
+print("la", end=" ")
+print("misma", end=" ")
+print("línea")
+
+#Ejemplo como imprimir una lista de números en la misma línea separados por un -
+print(1,2,3,4,5, sep="-", end=" :) ")
