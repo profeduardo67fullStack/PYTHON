eeee6677ac76c4dee52ab4befdaaf916c079c861
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-08 14:57:19 -0600

    Prácticando if-else e if-elif-else

---
+++ EstructurasCondicionales.py
@@ -0,0 +1,17 @@
+numero=(int(input("Escriba un número entero: ")))
+
+if(numero%2==0):
+    print("El número es par")
+else:
+    print("El número es impar")
+
+# Tres casos para las comparaciones
+
+x=11
+if x>10:
+    print("x es mayor que 10")
+elif x<10:
+    print("x es menor que 10")
+else:
+    print("x es igual que 10")
+
