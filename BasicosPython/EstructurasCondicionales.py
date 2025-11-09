28fa099317072cd85cb1ce2725d879113a64498d
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-09 10:11:40 -0600

    Moviendo archivos a carpeta básicos

---
+++ BasicosPython/EstructurasCondicionales.py
@@ -0,0 +1,24 @@
+'''
+No: 5
+Tema: Uso de if-else e if-elfi-else
+Autor: Prof. Eduardo67
+Fecha: 08/11/2025
+'''
+
+numero=(int(input("Escriba un número entero: ")))
+
+if(numero%2==0):
+    print("El número es par")
+else:
+    print("El número es impar")
+
+# Tres casos para las comparaciones
+
+x=11
+if x>10:
+    print("x es mayor que 10")
+elif x<10:
+    print("x es menor que 10")
+else:
+    print("x es igual que 10")
+
