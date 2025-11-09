f3542a27a38a4174ef4bae879203ee6287a50149
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-09 10:34:49 -0600

    Mover archivo a básicos

---
+++ BasicosPython/CicloWhile.py
@@ -0,0 +1,12 @@
+'''
+No: 8
+Tema: Practicando bucle while
+Autor: Prof. Eduardo67
+Fecha: 09/11/2025
+'''
+
+numero=int(input("Introduzca un númeor positivo: "))
+while numero<0:
+    print("Ha escrito un npumero negativo inténtelo de nuevo")
+    numero=int(input("Introduzca un númeor positivo: "))
+print("Gracias por su colaboración")
