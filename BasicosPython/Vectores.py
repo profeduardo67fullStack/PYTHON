bb2b483a9805942a0c0a5c3e20fd795cc73ae988
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-14 16:38:30 -0600

    Agregando algunas operaciones con vectores

--- BasicosPython/Vectores.py
+++ BasicosPython/Vectores.py
@@ -11,3 +11,42 @@ nombres=[["Pedro", "Juán", "Reynaldo"], "María",1,2,3,[True,23,"Eduardo"]]
 
 print(nombre)
 print(nombres)
+
+#Operaciones básicas sobre arreglos
+
+vector=[1,3,5] #Declaramos e inicializamos un vector
+print(vector)
+
+#Mostrar índices específicos
+print(vector[0])
+print(vector[1])
+print(vector[2])
+
+#Asignar valores específicos por índice
+vector[0]=7
+vector[1]=6
+vector[2]=5
+
+print(vector)
+
+print(vector[-1])
+print(vector[-2])
+print(vector[-3])
+
+#Duplicando el contenido de un vector
+
+vector=vector+vector
+print(vector)
+
+vector=vector*2
+print(vector)
+
+#Recorrer un vector con for
+
+for item in vector:
+    print(item)
+
+#Acceder al vector y al mismo tiempo multiplicarlo por 2
+for item in vector:
+    print(item*2)
+
