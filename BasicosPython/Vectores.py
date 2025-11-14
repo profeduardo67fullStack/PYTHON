b0353235c345705fbef6ddf27aa98ad116399842
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-14 15:32:35 -0600

    Uso de vectores básicos en Python

---
+++ BasicosPython/Vectores.py
@@ -0,0 +1,13 @@
+'''
+No: 17
+Tema: Practicando Vectores
+multiplica por 2. Se sumarán los multiplicados que correspondan a multiplicadores impares.
+Autor: Prof. Eduardo67
+Fecha: 09/11/2025
+'''
+
+nombre=["Pedro","Juan","María","Manuel"]
+nombres=[["Pedro", "Juán", "Reynaldo"], "María",1,2,3,[True,23,"Eduardo"]]
+
+print(nombre)
+print(nombres)
