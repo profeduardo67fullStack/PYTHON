9790037e8bfe41f7d8c5a0157d708e97c644484f
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-15 13:31:31 -0600

    Probando Diccionarios, basicos

---
+++ BasicosPython/Diccionarios.py
@@ -0,0 +1,19 @@
+'''
+No: 19
+Tema: Diccionarios
+Autor: Prof. Eduardo67
+Fecha: 15/11/2025
+'''
+
+diccionario={'a':1,'b':'hola',1:True}
+print(diccionario)
+print(diccionario['b'])
+#Reemplazr el valor de la clave 1
+diccionario[1]='valor nuevo'
+
+#Elimina un valor del diccionario
+
+del diccionario['a']
+print(diccionario)
+
+
