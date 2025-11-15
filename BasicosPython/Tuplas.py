ce8851cbd8f1a0629fa311bc4e9d5b63f03de243
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-15 13:01:41 -0600

    Ejemplo básico de una Tupla

---
+++ BasicosPython/Tuplas.py
@@ -0,0 +1,16 @@
+'''
+No: 19
+Tema: Listas
+Autor: Prof. Eduardo67
+Fecha: 14/11/2025
+'''
+
+#Declaración de una Tupla, es inmutable, no podemos cambiar sus valores en tiempo de ejecución
+#En la tupla no se dispone de métodos, es más rápido el acceso a una Tupla, que a una lista
+
+tupla=('a','b','c')
+print(tupla)
+
+#Tambien se accede a los elementos de la Tupla por su índice
+
+print(tupla[1])
