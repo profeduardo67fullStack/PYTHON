5526f60629ce474aa4cb518e26ef88f4a33dfb73
profeduardo67fullStack <profeduardo67.cap@gmail.com>
2025-11-15 14:10:18 -0600

    Básicos con cadenas de texto

---
+++ BasicosPython/ManejoDeTexto.py
@@ -0,0 +1,47 @@
+'''
+No: 20
+Tema: Manejo de cadenas de caracteres
+Autor: Prof. Eduardo67
+Fecha: 15/11/2025
+'''
+
+cadena="HOLA"
+print(cadena[0])
+print(cadena[1:2])
+print(cadena[1:3])
+print(cadena[3]) 
+
+
+print('Esta es una cadena entre comillas sencillas')
+print("Esta es una cadena entre comillas dobles")
+print("""Esta es una cadena entre comillas dobles""")
+        
+
+print ('Esta es una "cadena" entre comillas sencillas')
+print("Esta es una cadena entre 'comillas dobles'")
+print("""Esta es "una"
+       cadena 'entre comillas '
+       triples""")
+        
+
+#print('Esta es una 'cadena' entre comillas sencillas') marcaría error
+#print("Esta es una cadena entre "comillas dobles"") marcaría error
+        
+print('Esta es una "cadena" entre comillas sencillas')
+print("Esta es una cadena entre 'comillas dobles'")
+        
+cadena1="Un \ndivertido\t"
+cadena2="programa\t"
+cadena3="de\t"
+cadena4="radio"
+print(cadena1+cadena2+cadena3+cadena4) 
+
+print(3*"programas ")
+print("python "*3) 
+
+
+palabra="bienvenido a python"
+print(type(palabra))
+print(dir(palabra))
+              
+
